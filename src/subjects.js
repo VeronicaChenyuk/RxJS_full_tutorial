89d15553294c7be607f4b85db18368c82e69920c
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-09-24 14:38:33 +0300

    Subject->ReplaySubject

---
+++ src/subjects.js
@@ -0,0 +1,11 @@
+const { Subject, BehaviorSubject, ReplaySubject } = require("rxjs");
+
+// import { Subject } from "rxjs";
+
+document.addEventListener("click", () => {
+  const stream$ = new ReplaySubject(2)
+  stream$.next("Hello")
+  stream$.next("Rx")
+  stream$.next("Js")
+  stream$.subscribe(v => console.log("Value", v))
+});
